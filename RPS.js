cb7cff80d94774efa33d0004a8e774935fdcfa2f
Nathan Crasto <nathancrasto6@gmail.com>
2023-04-15 14:54:24 +0400

    Init

---
+++ RPS.js
@@ -0,0 +1,49 @@
+
+function RPS(computerSelection, playerSelection) {
+    
+
+    if (computerSelection == playerSelection) {
+
+        return "Draw!"
+
+    } else if (
+    (playerSelection == 'rock' && computerSelection  == 'scissors') || 
+    (playerSelection == 'paper' && computerSelection  == 'rock') || 
+    (playerSelection == 'scissors' && computerSelection  == 'paper')) {
+
+        return "Player Wins!"
+
+    } else {
+
+        return "Computer Wins!"
+
+    }
+
+
+}
+
+function computerSelection() {
+    
+    var randomInt = Math.round((Math.random()*100) + 1);
+    if (randomInt % 3 == 0) {
+        
+        return 'rock';
+        
+    } else if (randomInt % 2 == 0) {
+
+        return 'scissors';
+    
+    } else {
+
+        return 'paper';
+    
+    }
+
+}
+
+
+const playerSelection = prompt('rock, paper, scissors').toLowerCase()
+const compSelection = computerSelection()
+console.log(playerSelection, compSelection, RPS(compSelection, playerSelection))
+
+
